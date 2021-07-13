6c1e6bb9940a247f80274fc74570873f1fefadd7
zhaowx <zhaoweixu@zg-sst.com>
2021-07-13 16:47:32 +0800

    解决打包后chunk-vendors.js太慢

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,30 @@
 const path = require("path");
+const webpack = require('webpack')
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const productionGzipExtensions = ['js', 'css']
+
 module.exports = {
+  lintOnSave: false,
+  runtimeCompiler: true,
+  publicPath: './', // 设置打包文件相对路径
+  // 这是前端解决跨域的代码
+  devServer: {
+    // open: process.platform === 'darwin',
+    // host: 'localhost',
+    port: 8080,
+    // open: true, //配置自动启动浏览器
+    // proxy: {
+    //   '/api': {
+    //     target: ``,//写上接口基地址
+    //     changeOrigin: true,
+    //     ws: true,
+    //     // secure: false, //如果是http接口，需要配置此参数
+    //     pathRewrite: {
+    //       '^/api': ''
+    //     }
+    //   }
+    // }
+  },
   configureWebpack: {
     resolve: {
       alias: {
@@ -11,9 +36,20 @@ module.exports = {
         utils: path.resolve('src/utils')
       },
     },
-  },
-  devServer: {
-    port: 8000,
-    open: true,
+    plugins: [
+      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+
+      // 下面是下载的插件的配置
+      new CompressionWebpackPlugin({
+        algorithm: 'gzip',
+        test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+        threshold: 10240,
+        minRatio: 0.8
+      }),
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 5,
+        minChunkSize: 100
+      })
+    ]
   },
 };
